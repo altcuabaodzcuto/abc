214c3bf1e70f236c4b101a414f1af816084cc66d
altcuabaodzcuto <tranvanbao14112008@gmail.com>
2024-07-20 10:34:06 +0700

    Update luau-obfuscated.lua

--- luau-obfuscated.lua
+++ luau-obfuscated.lua
@@ -1,10 +1,136 @@
+local function load()
+ 
+ repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+ wait(1)
+
 if game.PlaceId == 6897167394 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/altcuabaodzcuto/abc/main/FS.lua"))()
-elseif game.PlaceId == 4520749081 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/altcuabaodzcuto/abc/main/KL.lua"))()
+elseif game.PlaceId == 10260193230 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/altcuabaodzcuto/abc/main/MM.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ScriptLinhTinh/main/Scare.lua"))()
 end
+
+end
+
+local Links = 'ElgatoHub'
+local SavedKeyPath = "ELGATO HUB/SavedKey.txt"
+local KeyLibrary = KeyLibrary or loadstring(game:HttpGet('https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/setup_obf.lua'))()
+local KeySystem = KeyLibrary.new(Links)
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/Library/retard/Fluent.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "ELGATO HUB | KEY SYSTEM",
+    SubTitle = "by tvbcutodz",
+    TabWidth = 65,
+    Size = UDim2.fromOffset(299, 250),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.F15
+})
+local Tabs = {
+    Main = Window:AddTab({ Title = "Home", Icon = "rbxassetid://17542643869" }),
+}
+
+Tabs.Main:AddParagraph({
+    Title = "Game Supports: Fruit Seas",
+    Content = ""
+})
+
+Tabs.Main:AddParagraph({
+    Title = "Game Supports: King Legacy",
+    Content = ""
+})
+
+local function Notify(v)
+    if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        Fluent:Notify({
+            Title = "Elgato Hub | ✅ Working",
+            Content = v,
+            Duration = 5
+        })
+    else
+        Fluent:Notify({
+            Title = "Elgato Hub | ❎ Working",
+            Content = v,
+            Duration = 5
+        })
+    end
+end
+
+local function Notify1(v)
+        Fluent:Notify({
+            Title = "Elgato Hub",
+            Content = v,
+            Duration = 50
+        })
+end
+
+local function isKeyValid(keyInput)
+    if keyInput then
+        local isKeyValid = KeySystem:verifyKey(keyInput)
+        if isKeyValid then
+            if writefile then
+                writefile(SavedKeyPath, keyInput)
+            end
+            Notify("✅ Valid Key!")
+            wait(1)
+            load()
+            return true
+        else
+            Notify("❎ Invalid Key!")
+        end
+    end
+end
+
+if readfile and writefile then
+    local success_file, error_file = pcall(function()
+        local is_key_present = isfile(SavedKeyPath);
+        if is_key_present == true then
+            Notify("Checking Key...")
+            local key_file_txt = readfile(SavedKeyPath)
+            local onl_key = isKeyValid(key_file_txt)
+            if onl_key then
+                Notify("✅ Save Key")
+            else
+                Notify("❎ Save Key")
+                delfile(SavedKeyPath)
+            end
+        end
+    end)
+    if error_file then
+        Notify(error_file)
+    end
+end
+
+Tabs.Main:AddInput("Input", {
+    Title = "",
+    Default = "",
+    Placeholder = "PUT YOUR KEY",
+    Numeric = false,
+    Finished = false,
+    Callback = function(va)
+        keyTextbox = va
+    end
+})
+
+Tabs.Main:AddButton({
+    Title = "CHECK KEY",
+    Description = "",
+    Callback = function()
+        isKeyValid(keyTextbox)
+    end
+})
+
+Tabs.Main:AddButton({
+    Title = "GET KEY",
+    Description = "",
+    Callback = function()
+        KeySystem:copyGetKeyURL()
+        Notify("Copy Success")
+    end
+})
+
 
 --anti afk
 local VirtualUser = game:GetService("VirtualUser")
