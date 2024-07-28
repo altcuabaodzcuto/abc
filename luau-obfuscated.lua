f4aad3b997f63e6e5859eb53da14e2d74b7a2cf5
altcuabaodzcuto <tranvanbao14112008@gmail.com>
2024-07-28 21:49:04 +0700

    Update luau-obfuscated.lua

--- luau-obfuscated.lua
+++ luau-obfuscated.lua
@@ -12,84 +12,29 @@ end
 
 end
 
-local ServiceID = "elgatohub"
-local PandaAuth = loadstring(game:HttpGet("https://raw.githubusercontent.com/luableapi/main/main/panda.lua"))()
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/Library/retard/Fluent.lua"))()
+local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/altcuabaodzcuto/abc/main/Pixel.lua"))()
 
--- Create the main window
-local Window = Fluent:CreateWindow({
-    Title = "ELGATO HUB | PELIDAN COMMUNITY KEY SYSTEM",
-    SubTitle = "VER 2.6",
-    TabWidth = 65,
-    Size = UDim2.fromOffset(350, 250),
-    Acrylic = false,
-    Theme = "Darker",
-    MinimizeKey = Enum.KeyCode.F15
+KeySys.Main({
+  HubName = "Elgato Hub",
+  PandaAuth = loadstring(game:HttpGet('https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/library/LuaLib/ROBLOX/PandaBetaLib.lua'))(),
+  Discord = "https://discord.gg/UrkE64Mfb7",
+  Service = "elgatohub",
+  APIToken = "", --Only for V4 support Themes(Maggix)
+  CustomTextBoxMessage = nil, --put a string if u want
+  KeylessSettings = {
+  	LoopKeyless = {
+  	  Active = true,
+        Interval = 5
+       },
+        CorrectHWID = "CORRECT HWID",
+        IncorrectHWID = "INCORRECT HWID",
+       PremiumEnabled = true,
+       SavePremium = true 
+   },
+  NormalScript = function()
+   load()
+  end,
+  PremiumScript = function()
+    load()
+  end
 })
-
--- Define tabs
-local Tabs = {
-    Main = Window:AddTab({ Title = "Home", Icon = "rbxassetid://17542643869" }),
-}
-
--- Define a notification function
-local function Notify(v)
-    Fluent:Notify({
-        Title = "ELGATO HUB",
-        Content = v,
-        Duration = 5
-    })
-end
-
-Tabs.Main:AddParagraph({
-    Title = "AUTO WHITELST",
-    Content = "SUPPORT ONLY WHITELIST"
-})
-
-Tabs.Main:AddButton({
-    Title = "GET LINK WHITELIST",
-    Description = "",
-    Callback = function()
-        setclipboard(PandaAuth:GetKey(ServiceID))
-    end
-})
-
-Tabs.Main:AddParagraph({
-    Title = "PREMIUM KEY",
-    Content = "SUPPORT ONLY PREMIUM KEY"
-})
-
-Tabs.Main:AddInput("Input", {
-    Title = "KEY",
-    Default = "",
-    Placeholder = "BAOCUTO",
-    Numeric = false,
-    Finished = false,
-    Callback = function(va)
-        _G.Key = va
-    end
-})
-
-Tabs.Main:AddButton({
-    Title = "CHECK KEY",
-    Description = "",
-    Callback = function()
-        if PandaAuth:ValidatePremiumKey(ServiceID, _G.Key) then
-            load()
-            Notify("Authenticated")
-        else
-            Notify("Not Authenticated")
-        end
-    end
-})
-
-while true do
-    if PandaAuth:Authenticate_Keyless(ServiceID) then
-        load()
-        Notify("Successfully Authorized")
-        break
-    else
-        Notify("Hardware ID not Successfully Authorized")
-    end
-    wait(5)
-end
