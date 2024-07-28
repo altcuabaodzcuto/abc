b43179924b915246bc92ff37e63a372311daa70e
altcuabaodzcuto <tranvanbao14112008@gmail.com>
2024-07-29 00:00:31 +0700

    Update luau-obfuscated.lua

--- luau-obfuscated.lua
+++ luau-obfuscated.lua
@@ -12,29 +12,148 @@ end
 
 end
 
-local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/EZ-KeySystem/main/V2/EzKeySystem.lua"))()
+local Links = 'ElgatoHub'
+local SavedKeyPath = "ELGATO HUB/SavedKey.txt"
+local KeyLibrary = KeyLibrary or loadstring(game:HttpGet('https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/setup_obf.lua'))()
+local KeySystem = KeyLibrary.new(Links)
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-KeySys.Main({
-  HubName = "Elgato Hub",
-  PandaAuth = loadstring(game:HttpGet('https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/library/LuaLib/ROBLOX/PandaBetaLib.lua'))(),
-  Discord = "https://discord.gg/UrkE64Mfb7",
-  Service = "elgatohub",
-  APIToken = "", --Only for V4 support Themes(Maggix)
-  CustomTextBoxMessage = nil, --put a string if u want
-  KeylessSettings = {
-  	LoopKeyless = {
-  	  Active = true,
-        Interval = 5
-       },
-        CorrectHWID = "CORRECT HWID",
-        IncorrectHWID = "INCORRECT HWID",
-       PremiumEnabled = true,
-       SavePremium = true 
-   },
-  NormalScript = function()
-   load()
-  end,
-  PremiumScript = function()
-    load()
-  end
-})
+local function Notify(v)
+    Fluent:Notify({
+        Title = "ELGATO HUB",
+        Content = v,
+        Duration = 5
+    })
+end
+
+local function isKeyValid(keyInput)
+    if keyInput then
+        local isKeyValid = KeySystem:verifyKey(keyInput)
+        if isKeyValid then
+            if writefile then
+                writefile(SavedKeyPath, keyInput)
+            end
+            Notify("VALID KEY!")
+            wait(1)
+            load()
+            return true
+        else
+            Notify("INVALID KEY!")
+        end
+    end
+end
+
+if readfile and writefile then
+    local success_file, error_file = pcall(function()
+        local is_key_present = isfile(SavedKeyPath);
+
+        if is_key_present == true then
+            Notify("CHECKING SAVED KEY...")
+
+            local key_file_txt = readfile(SavedKeyPath)
+            local onl_key = isKeyValid(key_file_txt)
+
+            if onl_key then
+                Notify("VALID KEY!")
+            else
+                Notify("UNVALID KEY")
+                delfile(SavedKeyPath)
+            end
+        end
+    end)
+    if error_file then
+        Notify("FAILED TO CHECK SAVED KEY")
+        warn(error_file)
+    end
+end
+
+
+local Main = Instance.new("ScreenGui", gethui())
+Main.Name = "Main"
+Main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+local KeySys = Instance.new("CanvasGroup", Main)
+KeySys.GroupTransparency = 0.2
+KeySys.BorderSizePixel = 0
+KeySys.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+KeySys.AnchorPoint = Vector2.new(0.5, 0.5)
+KeySys.Size = UDim2.new(0, 400, 0, 210)
+KeySys.Position = UDim2.new(0.5, 0, 0.5, 0)
+KeySys.BorderColor3 = Color3.fromRGB(0, 0, 0)
+KeySys.Name = "KeySys"
+
+local UICorner = Instance.new("UICorner", KeySys)
+
+local UIStroke = Instance.new("UIStroke", KeySys)
+UIStroke.Color = Color3.fromRGB(255, 255, 255)
+
+local whitelist = Instance.new("TextBox", KeySys)
+whitelist.BorderSizePixel = 2
+whitelist.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+whitelist.TextSize = 12
+whitelist.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+whitelist.TextColor3 = Color3.fromRGB(201, 201, 201)
+whitelist.BackgroundTransparency = 0.9
+whitelist.Size = UDim2.new(0, 336, 0, 129)
+whitelist.BorderColor3 = Color3.fromRGB(0, 0, 0)
+whitelist.Text = ""
+whitelist.PlaceholderText = "PUT YOUR KEY"
+whitelist.Name = "whitelist"
+whitelist.Position = UDim2.new(0.08, 0, 0.16942, 0)
+
+local whitelistUIStroke = Instance.new("UIStroke", whitelist)
+whitelistUIStroke.Thickness = 0.2
+
+local Frame = Instance.new("Frame", KeySys)
+Frame.BorderSizePixel = 0
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Size = UDim2.new(0, 400, 0, 19)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+
+local TextLabel = Instance.new("TextLabel", Frame)
+TextLabel.BorderSizePixel = 0
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 15
+TextLabel.FontFace = Font.new("rbxasset://fonts/families/Inconsolata.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.Size = UDim2.new(0, 400, 0, 19)
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.Text = "ELGATO HUB KEY SYSTEM"
+
+local getkey = Instance.new("TextButton", KeySys)
+getkey.BorderSizePixel = 0
+getkey.TextSize = 20
+getkey.TextColor3 = Color3.fromRGB(255, 255, 255)
+getkey.BackgroundColor3 = Color3.fromRGB(101, 101, 121)
+getkey.FontFace = Font.new("rbxasset://fonts/families/ComicNeueAngular.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+getkey.Size = UDim2.new(0, 136, 0, 32)
+getkey.Name = "getkey"
+getkey.BorderColor3 = Color3.fromRGB(0, 0, 0)
+getkey.Text = "GET KEY"
+getkey.Position = UDim2.new(0.1375, 0, 0.83, 0)
+getkey.MouseButton1Click:Connect(function()
+    KeySystem:copyGetKeyURL()
+    Notify("COPY SUCCESS")
+end)
+
+local login = Instance.new("TextButton", KeySys)
+login.BorderSizePixel = 0
+login.TextSize = 20
+login.TextColor3 = Color3.fromRGB(255, 255, 255)
+login.BackgroundColor3 = Color3.fromRGB(101, 101, 121)
+login.FontFace = Font.new("rbxasset://fonts/families/ComicNeueAngular.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+login.Size = UDim2.new(0, 136, 0, 32)
+login.Name = "login"
+login.BorderColor3 = Color3.fromRGB(0, 0, 0)
+login.Text = "CHECK KEY"
+login.Position = UDim2.new(0.51, 0, 0.83, 0)
+login.MouseButton1Click:Connect(function()
+    isKeyValid(whitelist.Text)
+end)
+
+local getkeyUICorner = Instance.new("UICorner", getkey)
+
+local loginUICorner = Instance.new("UICorner", login)
+
+local FrameUIStroke = Instance.new("UIStroke", Frame)
+FrameUIStroke.Color = Color3.fromRGB(255, 255, 255)
